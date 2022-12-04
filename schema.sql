3e5b598d23a67bcf2f6cd0d0c3c5c58b7feea607
jituizi77 <116607912+jituizi77@users.noreply.github.com>
2022-12-04 02:35:11 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,4 +1,48 @@
 
+        CREATE TABLE IF NOT EXISTS hourly_weather
+        (
+            id INTEGER PRIMARY KEY,
+            DATE DATE,
+            HourlyPrecipitation FLOAT,
+            HourlyWindSpeed FLOAT
+            );
+
+         CREATE TABLE IF NOT EXISTS daily_weather
+        (
+            id INTEGER PRIMARY KEY,
+            DATE DATE,
+            Precipitation FLOAT,
+            Windspeed FLOAT
+            );
+
+
+        CREATE TABLE IF NOT EXISTS taxi_trips
+        (
+            id INTEGER PRIMARY KEY,
+            pickup_datetime DATE,
+            dropoff_datetime DATE,
+            tip_amount FLOAT,
+            pickup_longitude FLOAT,
+            pickup_latitude FLOAT,
+            dropoff_longitude FLOAT,
+            dropoff_latitude FLOAT,
+            distance FLOAT
+            
+        );
+        
+
+    CREATE TABLE IF NOT EXISTS uber_trips
+        (
+            id INTEGER PRIMARY KEY,
+            pickup_datetime DATE,
+            pickup_longitude FLOAT,
+            pickup_latitude FLOAT,
+            dropoff_longitude FLOAT,
+            dropoff_latitude FLOAT,
+            distance FLOAT
+            
+        );
+
          CREATE TABLE IF NOT EXISTS daily_sunrise_sunset
         (
             id INTEGER PRIMARY KEY,
